3dd0353ba2d7d08b7e1ec05cc00193b85a72ddaa
Sean Chen <skypemaster007@gmail.com>
2014-07-25 16:57:57 -0700

    Added blank doublyLinkedList.js file and corresponding spec file.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,52 +1,52 @@
 var makeBinarySearchTree = function(value) {
-	var newBST = {};
-	newBST.value = value;
-	newBST.left = undefined;
-	newBST.right = undefined;
-	_.extend(newBST, bstMethods);
-	return newBST;
+  var newBST = {};
+  newBST.value = value;
+  newBST.left = undefined;
+  newBST.right = undefined;
+  _.extend(newBST, bstMethods);
+  return newBST;
 };
 
 var bstMethods = {};
 
 bstMethods.insert = function(value) {
   if (value < this.value) {
-  	if (this.left === undefined) {
-  		this.left = makeBinarySearchTree(value);
-  	} else {
-  		this.left.insert(value);
-  	}
+    if (this.left === undefined) {
+      this.left = makeBinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
   } else if (value > this.value) {
-  	if (this.right === undefined) {
-  		this.right = makeBinarySearchTree(value);
-  	} else {
-  		this.right.insert(value);
-  	}
+    if (this.right === undefined) {
+      this.right = makeBinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
   }
 };
 
 bstMethods.contains = function(value) {
   if (value === this.value) {
-  	return true;
+    return true;
   } else if (value < this.value) {
-  	if (this.left !== undefined) {
-  		return this.left.contains(value);
-  	}
+    if (this.left !== undefined) {
+      return this.left.contains(value);
+    }
   } else if (value > this.value) {
-  	if (this.right !== undefined) {
-  		return this.right.contains(value);
-  	}
+    if (this.right !== undefined) {
+      return this.right.contains(value);
+    }
   }
   return false;
 };
 
 bstMethods.depthFirstLog = function(callback) {
-	callback(this.value);
+  callback(this.value);
   if(this.left !== undefined){
-  	this.left.depthFirstLog(callback);
+    this.left.depthFirstLog(callback);
   }
   if(this.right !== undefined){
-  	this.right.depthFirstLog(callback);
+    this.right.depthFirstLog(callback);
   }
 };
 
---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,34 @@
+var makeDoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToHead = function(value) {
+
+  };
+
+  list.addToTail = function(value) {
+
+  };
+
+  list.removeHead = function() {
+
+  };
+
+  list.removeTail = function() {
+
+  };
+
+  list.contains = function(target) {
+
+  };
+
+};
+
+var makeNode = function(value) {
+  var node = {};
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+  return node;
+};
