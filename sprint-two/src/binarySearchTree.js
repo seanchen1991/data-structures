c8531eeca8c115f4d00b729eb76193da7f203d3b
Sean Chen <skypemaster007@gmail.com>
2014-07-25 08:55:40 -0700

    Added binarySearchTree.js.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,54 @@
-var makeBinarySearchTree = function(value){
+var makeBinarySearchTree = function(value) {
+	var newBST = {};
+	newBST.value = value;
+	newBST.left = undefined;
+	newBST.right = undefined;
+	_.extend(newBST, bstMethods);
+	return newBST;
 };
 
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+  if (value < this.value) {
+  	if (this.left === undefined) {
+  		this.left = makeBinarySearchTree(value);
+  	} else {
+  		this.left.insert(value);
+  	}
+  } else if (value > this.value) {
+  	if (this.right === undefined) {
+  		this.right = makeBinarySearchTree(value);
+  	} else {
+  		this.right.insert(value);
+  	}
+  }
+};
+
+bstMethods.contains = function(value) {
+  if (value === this.value) {
+  	return true;
+  } else if (value < this.value) {
+  	if (this.left !== undefined) {
+  		return this.left.contains(value);
+  	}
+  } else if (value > this.value) {
+  	if (this.right !== undefined) {
+  		return this.right.contains(value);
+  	}
+  }
+  return false;
+};
+
+bstMethods.depthFirstLog = function(callback) {
+	callback(this.value);
+  if(this.left !== undefined){
+  	this.left.depthFirstLog(callback);
+  }
+  if(this.right !== undefined){
+  	this.right.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,33 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  if (this.children === undefined) {
+  	this.children = [];
+  }
+  this.children[this.children.length] = makeTree(value);
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target) {
+  	return true;
+  } else {
+  	if (this.children) {
+  		for (var i = 0, x = this.children.length; i < x; i++) {
+  			if (this.children[i].contains(target)){
+  			  return true;
+  			}
+  		}
+  		return false;
+  	}
+  	return false;
+  }
 };
 
 
