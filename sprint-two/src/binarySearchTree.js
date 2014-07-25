abb803e0b643f10efa3aecdd2cb691c3c5f4fbc8
seanchen1991 <skypemaster007@gmail.com>
2014-07-25 16:14:06 -0700

    Added graph.js.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,21 +1,58 @@
 var makeBinarySearchTree = function(value){
-  var newBST = {};
-  list.left = null;
-  list.right = null;
-
-  newBST.insert = function(value) {
-
-  };
-
-  newBST.contains = function(value) {
-
-  };
-
-  newBST.depthFirstLog = function(callback) {
-
-  };
-  return newBST;
+  var node = {};
+  node.value = value;
+  node.left = undefined;
+  node.right = undefined;
+  _.extend(node, nodeMethods);
+  return node;
 };
+
+var nodeMethods = {};
+
+nodeMethods.insert = function(value) {
+  if(value <= this.value){
+    if(this.left !== undefined){
+      this.left.insert(value);
+    }else{
+      this.left = makeBinarySearchTree(value);
+    }
+  }else{
+    if(this.right !== undefined){
+      this.right.insert(value);
+    }else{
+      this.right = makeBinarySearchTree(value);
+    }
+  }
+};
+
+nodeMethods.contains = function(value) {
+  if(value === this.value){
+    return true;
+  }
+  if(this.left !== undefined){
+    if(this.left.contains(value)){
+      return true;
+    }
+  }
+  if(this.right !== undefined){
+    if(this.right.contains(value)){
+      return true;
+    }
+  }
+  return false;
+};
+
+nodeMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  if(this.left !== undefined){
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right !== undefined){
+    this.right.depthFirstLog(callback);
+  }
+};
+
+
 
 
 /*
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,22 +1,106 @@
 var Graph = function(){
+  this._collection = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  var counter = 0, newVertex = makeVertex(newNode);
+  this._collection[newNode] = newVertex;
+  if (toNode !== undefined) {
+    this.addEdge(newNode, toNode);
+  }else{
+    for (var key in this._collection) {
+      counter++;
+    }
+    if (counter === 2) {
+      var node1;
+      for(var key in this._collection){
+        if(key !== newNode){
+          node1 = key;
+        }
+      }
+      this.addEdge(newNode, node1);
+    }
+  }
 };
 
 Graph.prototype.contains = function(node){
+  for (var key in this._collection) {
+    if (key === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  var removedVertex = this._collection[node];
+  if (removedVertex !== undefined) {
+    delete this._collection[node];
+    var adjacent = removedVertex.list[0];
+    for (var i = 0, x = removedVertex.list.length; i < x; i++) {
+      if (removedVertex.list[i] === removedVertex.value) {
+        removedVertex.list[i] = undefined;
+      }
+    }
+  }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  var fromVertex = this._collection[fromNode];
+  for (var i = 0, x = fromVertex.list.length; i < x; i++) {
+    if (fromVertex.list[i] === toNode) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  var fromVertex = this._collection[fromNode];
+  var toVertex = this._collection[toNode];
+  if(fromVertex !== undefined && toVertex !== undefined){
+    fromVertex.list[fromVertex.list.length] = toNode;
+    toVertex.list[toVertex.list.length] = fromNode;
+  }
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var fromVertex = this._collection[fromNode];
+  var toVertex = this._collection[toNode];
+  if(fromVertex !== undefined && toVertex !== undefined){
+    var definedForFrom = false;
+    var definedForTo = false;
+    for(var i = 0; i < fromVertex.list.length; i++){
+      if(fromVertex.list[i] === toNode){
+        fromVertex.list[i] = undefined;
+      }
+      if(fromVertex.list[i] !== undefined) {
+        definedForFrom = true;
+      }
+    }
+    for(var i = 0; i < toVertex.list.length; i++){
+     if(toVertex.list[i] === fromNode){
+        toVertex.list[i] = undefined;
+      }
+      if (toVertex.list[i] !== undefined) {
+        definedForTo = true;
+      }
+    }
+    if (!definedForTo) {
+      delete this._collection[toNode];
+    }
+    if (!definedForFrom) {
+      delete this._collection[fromNode];
+    }
+  }
+};
+
+var makeVertex = function(node) {
+  var vertex = {
+    value: node,
+    list: []
+  };
+  return vertex;
 };
 
 /*
